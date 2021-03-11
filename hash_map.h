785524d90d9bf0ba9f5ecd98ed0d9d198fb869ec
Камалов Амир <kamalov.amir.02@gmail.com>
2021-03-11 14:09:37 +0300

    fixed ~15 conversations

--- hash_map.h
+++ hash_map.h
@@ -4,16 +4,24 @@
 #include <initializer_list>
 #include <list>
 #include <stdexcept>
+
 template<class KeyType, class ValueType, class Hash = std::hash<KeyType> >
 class HashMap {
 private:
+    // во сколько раз увеличится размер вектора, когда
+    // количество элементов станет больше, чем количество bucket'ов
+    // возможно если его изменить, то все поломается
+    static const int resize_value = 2;  
+    // а вот это значение точно можете менять как вам захочется(но только не 0)
+    static const size_t baseCapacity = 10;
     std::vector<std::list<std::pair<const KeyType, ValueType>>> data;
-    size_t values_number = 0;
+    size_t value_count = 0;
     Hash hasher;
-    void reallocation(size_t new_size) {
+
+    void reallocate(size_t new_size) {
         std::vector<std::list<std::pair<const KeyType, ValueType>>> new_data(new_size);
         std::swap(data, new_data);
-        values_number = 0;
+        value_count = 0;
         for (const auto &bucket : new_data) {
             for (const auto &value: bucket) {
                 insert(value);
@@ -22,22 +30,24 @@ private:
     }
 
 public:
-    HashMap(Hash _hasher = Hash())
+    explicit HashMap(Hash _hasher = Hash())
     : hasher(_hasher) {
-        data.resize(1);
+        data.resize(baseCapacity);
     }
+
     template<typename iter>
     HashMap(iter begin, iter end, Hash _hasher = Hash())
     : hasher(_hasher) {
         hasher = _hasher;
-        data.resize(1);
+        data.resize(baseCapacity);
         while (begin != end) {
             insert(*begin++);
         }
     }
+
     HashMap(const std::initializer_list<std::pair<const KeyType, ValueType>>& out, Hash _hasher = Hash())
     : hasher(_hasher) {
-        data.resize(1);
+        data.resize(baseCapacity);
         auto iter = out.begin();
         while (iter != out.end()) {
             insert(*iter++);
@@ -69,61 +79,67 @@ public:
         }
         return *this;
     }
+
     void insert(const std::pair<const KeyType, ValueType>& value) {
-        size_t pos = hasher(value.first) % data.size();
-        bool was_inserted = false;
-        for (auto& all : data[pos]) {
+        size_t bucket_id = hasher(value.first) % data.size();
+        bool value_copy_exists = false;
+        for (const auto& all : data[bucket_id]) {
             if (all.first == value.first) {
-                was_inserted = true;
+                value_copy_exists = true;
                 // wrong, because
                 // HashMap{{1, 1}, {1, 2}} = {1, 1}
                 // all.second = value.second;
                 break;
             }
         }
-        if (!was_inserted) {
-            data[pos].push_back(value);
-            ++values_number;
+        if (!value_copy_exists) {
+            data[bucket_id].push_back(value);
+            ++value_count;
         }
-        if (values_number > data.size()) {
-            reallocation(data.size() * 2);
+        if (value_count > data.size()) {
+            reallocate(data.size() * resize_value);
         }
     }
+
     size_t size() const {
-        return values_number;
+        return value_count;
     }
     bool empty() const {
-        return values_number == 0;
+        return value_count == 0;
     }
     Hash hash_function() const {
         return hasher;
     }
+
     void erase(const KeyType& key) {
-        size_t pos = hasher(key) % data.size();
+        size_t bucket_id = hasher(key) % data.size();
         bool has_deleted = false;
-        for (auto iter = data[pos].begin(); iter != data[pos].end(); ++iter) {
+        for (auto iter = data[bucket_id].begin(); iter != data[bucket_id].end(); ++iter) {
             if (iter->first == key) {
-                data[pos].erase(iter);
+                data[bucket_id].erase(iter);
                 has_deleted = true;
                 break;
             }
         }
         if (has_deleted) {
-            --values_number;
-            if (values_number == data.size() / 4 && (data.size() / 2)) {
-                reallocation(data.size() / 2);
+            --value_count;
+            if (value_count == data.size() / resize_value / 2 && (data.size() / resize_value)) {
+                reallocate(data.size() / resize_value);
             }
         }
     }
-    // <ITERATOR> 
+
+
+    // entry ITERATOR 
     using Pair = std::pair<const KeyType, ValueType>;
     class iterator {
         std::vector<std::list<Pair>>* data_pointer;
         typename std::vector<std::list<Pair>>::iterator bucket;
         typename std::list<Pair>::iterator value;
 
+
     public:
-        iterator() {}
+        iterator() = default;
         iterator(const iterator &other) {
             data_pointer = other.data_pointer;
             bucket = other.bucket;
@@ -136,6 +152,7 @@ public:
         : data_pointer(_data_pointer)
         , bucket(_bucket)
         , value(_value) {};
+
         iterator& operator++ () {
             if (bucket == data_pointer->end()) {
                 // if we actually at the end: do strange shit
@@ -150,7 +167,7 @@ public:
                         break;
                     }
                     if (bucket->begin() != bucket->end()) {
-                        // if list at least have 1 element
+                        // if list has at least 1 element
                         value = bucket->begin();
                         break;
                     }
@@ -158,11 +175,13 @@ public:
             }
             return *this;
         }
+
         iterator operator++ (int) {
             iterator old(*this);
             ++*this;
             return old;
         }
+
         bool operator== (const iterator& other) const {
             return (data_pointer == other.data_pointer
                 && bucket == other.bucket
@@ -171,16 +190,16 @@ public:
         bool operator!= (const iterator& other) const {
             return !(*this == other);
         }
+
         Pair& operator* () {
             return *value; 
         }
         typename std::list<Pair>::iterator operator-> () {
             return value;
         }
-
     };
     iterator begin() {
-        if (values_number != 0) {
+        if (value_count != 0) {
             for (auto iter = data.begin(); iter != data.end(); ++iter) {
                 if (iter->size() != 0) {
                     return iterator(&data, iter, iter->begin());
@@ -193,26 +212,27 @@ public:
         return iterator(&data, data.end(), data.begin()->begin());
     }
     iterator find(const KeyType& key) {
-        size_t pos = hasher(key) % data.size();
-        for (auto iter = data[pos].begin(); iter != data[pos].end(); ++iter) {
+        size_t bucket_id = hasher(key) % data.size();
+        for (auto iter = data[bucket_id].begin(); iter != data[bucket_id].end(); ++iter) {
             if (iter->first == key) {
-                return iterator(&data, data.begin() + pos, iter);
+                return iterator(&data, data.begin() + bucket_id, iter);
             }
         }
         return end();
     }
-    // <\ITERATOR>
+    // exit ITERATOR
 
 
 
-    // <CONST_ITERATOR> 
+    // entry CONST_ITERATOR 
     class const_iterator {
         const std::vector<std::list<Pair>>* data_pointer;
         typename std::vector<std::list<Pair>>::const_iterator bucket;
         typename std::list<Pair>::const_iterator value;
 
+
     public:
-        const_iterator() {}
+        const_iterator() = default;
         const_iterator(const const_iterator &other)
             : data_pointer(other.data_pointer)
             , bucket(other.bucket)
@@ -224,6 +244,7 @@ public:
         : data_pointer(_data_pointer)
         , bucket(_bucket)
         , value(_value) {};
+
         const const_iterator& operator++ () {
             if (bucket == data_pointer->end()) {
                 // if we actually at the end: do strange shit
@@ -246,6 +267,7 @@ public:
             }
             return *this;
         }
+
         const_iterator operator++ (int) {
             const_iterator old(*this);
             ++*this;
@@ -265,10 +287,10 @@ public:
         const typename std::list<Pair>::const_iterator operator-> () const {
             return value;
         }
-
     };
+
     const_iterator begin() const {
-        if (values_number != 0) {
+        if (value_count != 0) {
             for (auto iter = data.begin(); iter != data.end(); ++iter) {
                 if (iter->size() != 0) {
                     return const_iterator(&data, iter, iter->begin());
@@ -281,36 +303,38 @@ public:
         return const_iterator(&data, data.end(), data.begin()->begin());
     }
     const_iterator find(const KeyType& key) const {
-        size_t pos = hasher(key) % data.size();
-        for (auto iter = data[pos].begin(); iter != data[pos].end(); ++iter) {
+        size_t bucket_id = hasher(key) % data.size();
+        for (auto iter = data[bucket_id].begin(); iter != data[bucket_id].end(); ++iter) {
             if (iter->first == key) {
-                return const_iterator(&data, data.begin() + pos, iter);
+                return const_iterator(&data, data.begin() + bucket_id, iter);
             }
         }
         return end();
     }
-    // <\CONST_ITERATOR>
+    // exit CONST_ITERATOR
+
 
     ValueType& operator[] (const KeyType& key) {
-        size_t pos = hasher(key) % data.size();
-        for (auto& value : data[pos]) {
+        size_t bucket_id = hasher(key) % data.size();
+        for (auto& value : data[bucket_id]) {
             if (key == value.first) {
                 return value.second;
             }
         }
         insert({key, ValueType()});
-        pos = hasher(key) % data.size();
-        for (auto& value : data[pos]) {
+        bucket_id = hasher(key) % data.size();
+        for (auto& value : data[bucket_id]) {
             if (key == value.first) {
                 return value.second;
             }
         }
+        __builtin_unreachable();
         // never will be completed if insert is correct;
-        return *(new ValueType);
     }
+
     const ValueType& at(const KeyType& key) const {
-        size_t pos = hasher(key) % data.size();
-        for (const auto& value : data[pos]) {
+        size_t bucket_id = hasher(key) % data.size();
+        for (const auto& value : data[bucket_id]) {
             if (key == value.first) {
                 return value.second; 
             }
@@ -330,7 +354,7 @@ public:
     } */
     void clear() {
         data.clear();
-        data.resize(1);
-        values_number = 0;
+        data.resize(baseCapacity);
+        value_count = 0;
     }
 };
